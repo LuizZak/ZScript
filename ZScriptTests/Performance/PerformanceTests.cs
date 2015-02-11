731b6ee6635aab29f53951452415632c4d0c7c59
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2015-02-11 06:14:10 -0200

    Modifying performance tests

--- ZScriptTests/Performance/PerformanceTests.cs
+++ ZScriptTests/Performance/PerformanceTests.cs
@@ -37,7 +37,7 @@ namespace ZScriptTests.Performance
         [TestMethod]
         public void TestSubscriptPerformance()
         {
-            const string input = "func funca { var a = [0]; for(var i = 0; i < 10000; i++) { a[0] += i; } }";
+            const string input = "func funca { var a = [0]; for(var i = 0; i < 1000000; i++) { a[0] += i; } }";
 
             var generator = CreateGenerator(input);
 
