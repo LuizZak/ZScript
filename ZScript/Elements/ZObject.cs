6e6f088c31f883172b7cff54d676f7d729062f09
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2015-03-03 15:49:57 -0300

    Modifying ZObject class to be a direct subclass of the Dictionary class

--- ZScript/Elements/ZObject.cs
+++ ZScript/Elements/ZObject.cs
@@ -19,7 +19,6 @@
 */
 #endregion
 using System;
-using System.Collections;
 using System.Collections.Generic;
 
 using ZScript.Runtime.Execution.Wrappers;
@@ -29,13 +28,8 @@ namespace ZScript.Elements
     /// <summary>
     /// Specifies an dynamic object that can store values in subscripting and direct field access fashions
     /// </summary>
-    public class ZObject : ISubscripterWrapper, IDictionary
+    public class ZObject : Dictionary<string, object>, ISubscripterWrapper
     {
-        /// <summary>
-        /// Dictionary containing the objects stored in this ZObject class
-        /// </summary>
-        private readonly Dictionary<string, object> _dictionary = new Dictionary<string, object>();
-
         /// <summary>
         /// Gets or sets a value on this ZObject
         /// </summary>
@@ -47,13 +41,16 @@ namespace ZScript.Elements
             {
                 CheckType(indexer);
 
-                return _dictionary[(string)indexer];
+                if (!ContainsKey((string)indexer))
+                    return null;
+
+                return base[(string)indexer];
             }
             set
             {
                 CheckType(indexer);
 
-                _dictionary[(string)indexer] = value;
+                base[(string)indexer] = value;
             }
         }
 
@@ -79,125 +76,5 @@ namespace ZScript.Elements
         {
             return type == typeof(string);
         }
-
-        #region IDictionary implementation
-
-        // 
-        // IDictionary.Keys implementation
-        // 
-        public ICollection Keys
-        {
-            get { return _dictionary.Keys; }
-        }
-
-        // 
-        // IDictionary.Values implementation
-        // 
-        public ICollection Values
-        {
-            get { return _dictionary.Values; }
-        }
-
-        // 
-        // IDictionary.IsReadOnly implementation
-        // 
-        public bool IsReadOnly
-        {
-            get { return false; }
-        }
-
-        // 
-        // IDictionary.IsFixedSize implementation
-        // 
-        public bool IsFixedSize
-        {
-            get { return false; }
-        }
-
-        // 
-        // IDictionary.Contains implementation
-        // 
-        public bool Contains(object key)
-        {
-            CheckType(key);
-
-            return _dictionary.ContainsKey((string)key);
-        }
-
-        // 
-        // IDictionary.Add implementation
-        // 
-        public void Add(object key, object value)
-        {
-            this[key] = value;
-        }
-
-        // 
-        // IDictionary.Remove implementation
-        // 
-        public void Remove(object key)
-        {
-            CheckType(key);
-
-            _dictionary.Remove((string)key);
-        }
-
-        // 
-        // IDictionary.Clear implementation
-        // 
-        public void Clear()
-        {
-            _dictionary.Clear();
-        }
-
-        // 
-        // IDictionary.IDictionaryEnumerator implementation
-        // 
-        public IDictionaryEnumerator GetEnumerator()
-        {
-            return _dictionary.GetEnumerator();
-        }
-
-        // 
-        // IEnumerable.GetEnumerator implementation
-        // 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return ((IEnumerable)_dictionary).GetEnumerator();
-        }
-
-        // 
-        // ICollection.CopyTo implementation
-        // 
-        public void CopyTo(Array array, int index)
-        {
-            ((ICollection)_dictionary).CopyTo(array, index);
-        }
-
-        // 
-        // ICollection.Count implementation
-        // 
-        public int Count
-        {
-            get { return _dictionary.Count; }
-        }
-
-        // 
-        // ICollection.SyncRoot implementation
-        // 
-        public object SyncRoot
-        {
-            get { return ((ICollection)_dictionary).SyncRoot; }
-        }
-
-        // 
-        // ICollection.IsSynchronized implementation
-        // 
-        public bool IsSynchronized
-        {
-            get { return ((ICollection)_dictionary).IsSynchronized; }
-        }
-
-        #endregion
     }
 }
