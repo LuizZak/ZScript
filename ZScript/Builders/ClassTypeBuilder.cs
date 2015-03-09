b9545c0a82c3edd4b306f791e4e0f684a441d629
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2015-03-09 17:15:09 -0300

    Improving ClassTypeBuilder implementation

--- ZScript/Builders/ClassTypeBuilder.cs
+++ ZScript/Builders/ClassTypeBuilder.cs
@@ -114,48 +114,12 @@ namespace ZScript.Builders
         /// <summary>
         /// Fills a given IL generator with a set of tokens
         /// </summary>
-        /// <param name="generator">The generator to generate the IL on</param>
-        /// <param name="tokens">The body of tokens to generate the IL from</param>
-        private void FillGeneratorWithTokens(ILGenerator generator, TokenList tokens)
-        {
-            ParameterModifier[] mod = { new ParameterModifier(1) };
-            var constructor = typeof(ZClassInstance).GetConstructor(BindingFlags.Public | BindingFlags.Instance, Type.DefaultBinder, CallingConventions.Any, new[] { typeof(ZClass) }, mod);
-
-            if (constructor == null)
-            {
-                generator.Emit(OpCodes.Ret);
-                return;
-            }
-
-            // For a constructor, argument zero is a reference to the new
-            // instance. Push it on the stack before calling the base
-            // class constructor. Specify the default constructor of the 
-            // base class (System.Object) by passing an empty array of 
-            // types (Type.EmptyTypes) to GetConstructor.
-            generator.Emit(OpCodes.Ldarg_0);
-            generator.Emit(OpCodes.Call, constructor);
-
-            generator.Emit(OpCodes.Ret);
-        }
-
-        /// <summary>
-        /// Fills a given IL generator with a set of tokens
-        /// </summary>
         /// <param name="generator">The generator to generate the tokens on</param>
         /// <param name="constructor">The constructor containing the tokens to generate the IL from</param>
         private void FillConstructor(ILGenerator generator, MethodDefinition constructor)
         {
             ParameterModifier[] mod = { new ParameterModifier(1) };
             var zConst = typeof(ZClassInstance).GetConstructor(BindingFlags.Public | BindingFlags.Instance, Type.DefaultBinder, CallingConventions.Any, new[] { typeof(ZClass) }, mod);
-
-            if (zConst == null)
-            {
-                generator.Emit(OpCodes.Ldarg_0);
-                // ReSharper disable once AssignNullToNotNullAttribute
-                generator.Emit(OpCodes.Call, typeof(object).GetConstructor(Type.EmptyTypes));
-                generator.Emit(OpCodes.Ret);
-                return;
-            }
 
             // For a constructor, argument zero is a reference to the new
             // instance. Push it on the stack before calling the base
